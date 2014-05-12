9ea36598169fe2050db7ff54428b2831385e6a9b
bqc200 <bqc200@nyu.edu>
2014-05-11 23:38:09 -0400

    Create post_item.js

---
+++ client/views/posts/post_item.js
@@ -0,0 +1,16 @@
+Template.postItem.helpers({
+  ownPost: function() {
+    return this.userId == Meteor.userId();
+    },   
+   domain: function() {
+     var a = document.createElement('a');
+     a.href = this.url;
+     return a.hostname;
+ }
+});
+
+Template.postItem.events({
+ 'click button': function() {
+   Posts.update(this._id, {$inc : {likeCount : 1} });
+ }
+});
